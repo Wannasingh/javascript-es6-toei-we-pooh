9da8c0ee782c74bbec6b6dcfe7a48aef0bd09e10
Bunyawat Singkiporn <pupahero@gmail.com>
2024-10-15 14:45:52 +0700

    ex1-7

--- ex-5.js
+++ ex-5.js
@@ -1,6 +1,11 @@
-function sum() {
+function sum(...result) {
   // Start coding here !
-}
+  let total = 0;
+  for (let i=0; i<result.length; i++){
+    total = total + result[i];
+  }
+  return total;
+}//ข้อนี้จริงๆใช้.reduceได้เพราะต้องการหาผลรวม
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
 let result2 = sum(1, 2, 3, 4);
